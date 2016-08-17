8e37771cf95c324f4163c927868088ec858afe1f
Tashi Sherpa <tnsherpa1@gmail.com>
2016-08-17 16:02:08 -0700

    fixes call for price product view display and reference to controller action on slideshow module

--- app/code/Codazon/Slideshow/Controller/Adminhtml/Slideshow/InlineEdit.php
+++ app/code/Codazon/Slideshow/Controller/Adminhtml/Slideshow/InlineEdit.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Codazon\Slideshow\Controller\Adminhtml\Block;
+namespace Codazon\Slideshow\Controller\Adminhtml\Slideshow;
 
 use Magento\Backend\App\Action\Context;
 use Magento\Cms\Api\BlockRepositoryInterface as BlockRepository;
